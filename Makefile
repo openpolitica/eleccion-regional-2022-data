9722bd4bd8e621ad462e8edb863487a6013863ef
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2022-09-17 10:48:07 +0100

    add mvn, automate workflow

--- Makefile
+++ Makefile
@@ -2,6 +2,9 @@ all: candidatos detalle detalle_lima
 candidatos:
 	cat resultados.ndjson | jq -c | sqlite-utils insert elecciones.db candidatos --nl --flatten --truncate -
 
+run_detalle:
+	./mvnw exec:java -Dexec.mainClass="op.elecciones.AppDetalle"
+
 detalle:
 	cat resultados-filtrados.ndjson  | jq -c | sqlite-utils insert elecciones.db candidatos_detalle --nl --flatten --truncate --alter -
 	cat resultados-filtrados.ndjson  | jq -c | sqlite-utils insert elecciones.db candidatos_detalle_v2 --nl  --truncate --alter -
--- src/main/java/op/elecciones/AppDetalle.java
+++ src/main/java/op/elecciones/AppDetalle.java
@@ -49,60 +49,61 @@ public class AppDetalle {
     var httpClient = HttpClient.newBuilder().sslContext(sslContext).build();
     var json = new ObjectMapper();
 
-    var output = Path.of("resultados-filtrados.ndjson");
-    //var output = Path.of("resultados-filtrados-lima.ndjson");
-    if (!Files.exists(output)) Files.createFile(output);
-
-    var lines = Files.readAllLines(Path.of("filtrados.csv"));
-    //var lines = Files.readAllLines(Path.of("filtrados-lima.csv"));
-
-    final AtomicInteger loader = new AtomicInteger();
-    int onePercent = lines.size() / 100;
-
-    lines
-      .stream()
-      .dropWhile(s -> s.startsWith("idHojaVida"))
-      .map(line -> {
-        var fields = line.split(",");
-        return fields[0];
-      })
-      .map(idHojaVida ->
-        HttpRequest
-          .newBuilder()
-          .GET()
-          .header("Content-type", "application/json")
-          .uri(
-            URI.create(
-              "https://apiplataformaelectoral8.jne.gob.pe/api/v1/candidato/hoja-vida?IdHojaVida=" +
-              idHojaVida
-            )
-          )
-          .build()
-      )
-      .peek(httpRequest -> {
-        if (loader.incrementAndGet() % onePercent == 0) {
-          System.out.print(
-            loader.get() + " elements of " + lines.size() + " treated ("
-          );
-          System.out.println(((loader.get() / onePercent)) + "%)");
-        }
-      })
-      //      .parallel() // Too much for the backend
-      .forEach(httpRequest -> {
-        try {
-          var response = httpClient.send(
-            httpRequest,
-            HttpResponse.BodyHandlers.ofByteArray()
-          );
-          var jsonResponse = json.readTree(response.body());
-          Files.writeString(
-            output,
-            json.writeValueAsString(jsonResponse) + "\n",
-            APPEND
-          );
-        } catch (IOException | InterruptedException e) {
-          throw new RuntimeException(e);
-        }
-      });
+    scrape(httpClient, json, Path.of("resultados-filtrados.ndjson"), Path.of("filtrados.csv"));
+    scrape(httpClient, json, Path.of("resultados-filtrados-lima.ndjson"), Path.of("filtrados-lima.csv"));
   }
+
+    private static void scrape(HttpClient httpClient, ObjectMapper json, Path output, Path input) throws IOException {
+        var lines = Files.readAllLines(input);
+        if (!Files.exists(output)) Files.createFile(output);
+
+        final AtomicInteger loader = new AtomicInteger();
+        int onePercent = lines.size() / 100;
+
+        lines
+          .stream()
+          .dropWhile(s -> s.startsWith("idHojaVida"))
+          .map(line -> {
+            var fields = line.split(",");
+            return fields[0];
+          })
+          .map(idHojaVida ->
+            HttpRequest
+              .newBuilder()
+              .GET()
+              .header("Content-type", "application/json")
+              .uri(
+                URI.create(
+                  "https://apiplataformaelectoral8.jne.gob.pe/api/v1/candidato/hoja-vida?IdHojaVida=" +
+                  idHojaVida
+                )
+              )
+              .build()
+          )
+          .peek(httpRequest -> {
+            if (loader.incrementAndGet() % onePercent == 0) {
+              System.out.print(
+                loader.get() + " elements of " + lines.size() + " treated ("
+              );
+              System.out.println(((loader.get() / onePercent)) + "%)");
+            }
+          })
+          //      .parallel() // Too much for the backend
+          .forEach(httpRequest -> {
+            try {
+              var response = httpClient.send(
+                httpRequest,
+                HttpResponse.BodyHandlers.ofByteArray()
+              );
+              var jsonResponse = json.readTree(response.body());
+              Files.writeString(
+                      output,
+                json.writeValueAsString(jsonResponse) + "\n",
+                APPEND
+              );
+            } catch (IOException | InterruptedException e) {
+              throw new RuntimeException(e);
+            }
+          });
+    }
 }
